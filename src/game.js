40fa6b2f8c07af9d50bcd29a44ef5a2b632fb23f
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2024-03-31 21:53:34 +0200

    i fixe that

--- src/game.js
+++ src/game.js
@@ -1,52 +1,173 @@
-let aCards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56]
+let state = 0;
+let playerCount = 2;
+let doublePCount = playerCount * 2;
+let n = 52 + doublePCount;
+let aCards = [n];
+let cardsDealt = 0;
+let cardAmount = doublePCount;
+let j = cardAmount;
+let p1username = undefined;
+let p2username = undefined;
+let p3username = undefined;
+let p4username = undefined;
+
+for (i = n; i >= 1; i--) {
+    aCards[i] = i;
+}
+aCards.shift();
+
+function increasePlayerCount() {
+    if(playerCount <= 3) {
+        playerCount++;
+    }
+    doublePCount = playerCount * 2;
+    document.getElementById("playercount").innerHTML = "Player Count: " + playerCount;
+}
+
+function decreasePlayerCount() {
+    if(playerCount >= 3) {
+        playerCount--;
+    }
+    doublePCount = playerCount * 2;
+    document.getElementById("playercount").innerHTML = "Player Count: " + playerCount;
+}
+
+function refillCards() {
+    n = 52 + doublePCount;
+    aCards = [n];
+    for (i = n; i >= 1; i--) {
+        aCards[i] = i;
+    }
+    aCards.shift();
+}
 
 function pickCard() {
     let i;
     i = aCards.length;
-    let card = aCards[Math.floor(Math.random() * 56) % i];
+    let card = aCards[Math.floor(Math.random() * n) % i];
     let daqard = aCards.indexOf(card);
     aCards.splice(daqard, 1);
-    let norlequb = convertCard(card);
-    return norlequb
+    let cardID = convertCard(card);
+    return cardID
 }
 
 function convertCard(card) {
-    console.log(card)
-    let valor;
-    let caard = card % 4;
-    let ccard = card % 14;
-    caard++;
-    ccard++;
-    switch(caard) {
+    let suit;
+    let value;
+    value = Math.floor(card / 4);
+    value++;
+    if(card >= 53) {
+        value = "Joker";
+        suit = undefined;
+    }
+    if(value != "Joker") {
+        switch(value) {
+            case 1:
+                value = "Ace";
+                break;
+            case 11:
+                value = "Jack";
+                break;
+            case 12:
+                value = "Queen";
+                break;
+            case 13:
+                value = "King";
+                break;
+            case 14:
+                value = "Ace";
+                break;
+            case 15:
+                value = "Ace";
+            default:
+                break;
+        }
+    }
+    suit = card % 4;
+    suit++;
+    switch(suit) {
         case 1:
-            valor = "Spades";
+            suit = "Hearts";
             break;
         case 2:
-            valor = "Hearts";
+            suit = "Diamonds";
             break;
         case 3:
-            valor = "Clubs";
+            suit = "Clubs";
             break;
         case 4:
-            valor = "Diamonds";
+            suit = "Spades";
             break;
     }
-    let value = ccard
-    if(value == 14) {
-        value == "j";
-    }
-    let globble = {"valor":valor,"value":value}
-    return globble
+    let cardID = {"suit":suit, "value":value};
+    return cardID
 }
 
-function deal() {
-    j = 56;
-    while(true){
-        let car = pickCard();
-        console.log(car);
-        if(j == 1) {
+function generateCards() {
+    // button press
+    if(aCards.length <= doublePCount + 3) {
+        refillCards();
+    }
+    if(cardsDealt == 0) {
+        j = doublePCount;
+    } else if(cardsDealt == doublePCount) {
+        j = 3;
+    } else if(cardsDealt == doublePCount + 3) {
+        j = 1;
+    } else if(cardsDealt == doublePCount + 4) {
+        endRound();
+    }
+    while(true) {
+        if(j == 0) {
             break;
         }
+        let cadr = pickCard();
+        deal(cadr);
         j--;
     }
+    if(cardsDealt == doublePCount + 4) {
+        j = doublePCount;
+    }
+}
+
+function deal(card) {
+    cardsDealt++;
+    y = cardsDealt % playerCount;
+    if(card.value != "Joker") {
+        var name = card.value + " of " + card.suit;
+    } else {
+        var name = "Joker";
+    }
+    if(cardsDealt <= doublePCount) {
+        switch(y % doublePCount) {
+            case 0:
+                receiveCard("player1", name);
+                break;
+            case 1:
+                receiveCard("player2", name);
+                break;
+            case 2:
+                receiveCard("player3", name);
+                break;
+            case 3:
+                receiveCard("player4", name);
+                break;
+        }
+    } else {
+        receiveCard("table", name);
+    }
+}
+
+function receiveCard(player, card) {
+    document.getElementById(`${player}`).innerHTML += card + " | ";
+}
+
+function endRound() {
+    j = 0;
+    cardsDealt = 0;
+    document.getElementById("player1").innerHTML = "empty - ";
+    document.getElementById("player2").innerHTML = "empty - ";
+    document.getElementById("player3").innerHTML = "empty - ";
+    document.getElementById("player4").innerHTML = "empty - ";
+    document.getElementById("table").innerHTML = "table - ";
 }
