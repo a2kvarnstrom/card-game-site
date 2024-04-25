13cca31058402595aff6391e97e84d8d6a96d8c0
a2kvarnstrom <a2.kvarnstrom@gmail.com>
2024-04-25 15:30:06 +0200

    wrosk

--- src/game.js
+++ src/game.js
@@ -529,8 +529,8 @@ function endRound() {
     document.getElementById("betBar").addEventListener('mousedown', (e) => {
         isChoosing = true;
         [lastX, lastY] = [e.offsetX, e.offsetY];
-        choosebet();
     });
+    document.getElementById("betBar").addEventListener('mousedown', choosebet);
     document.getElementById("betBar").addEventListener('mousemove', choosebet);
     document.body.addEventListener('mouseup', () => isChoosing = false);
     cards = {
